ce4e46b5792cb839ada77f91a4db9dafa5912b7b
Nicolás Hock Isaza <nhocki@gmail.com>
2011-04-05 23:35:36 -0500

    Fixing RAILS type (is now Rails)

--- lib/generators/private_pub/templates/private_pub_initializer.rb
+++ lib/generators/private_pub/templates/private_pub_initializer.rb
@@ -1,3 +1,3 @@
-private_pub_config = YAML.load_file("#{RAILS.root.to_s}/config/private_pub.yml")[Rails.env]
+private_pub_config = YAML.load_file("#{Rails.root.to_s}/config/private_pub.yml")[Rails.env]
 PrivatePub.server = private_pub_config["server"]
 PrivatePub.secret_token = private_pub_config["secret_token"]
