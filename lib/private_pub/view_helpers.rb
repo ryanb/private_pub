b5c5ca8fa922f612be770fa75a600e0eb5618f39
Brandon Tilley <binarymuse@binarymuse.net>
2011-04-08 10:22:39 -0700

    Add ability to specify custom JavaScript callback:
    * modify subscribe_to to allow passing a :callback option
    * modify publish_to to allow passing an object to render to JSON
      instead of evaluating JavaScript

--- lib/private_pub/view_helpers.rb
+++ lib/private_pub/view_helpers.rb
@@ -1,17 +1,22 @@
 module PrivatePub
   module ViewHelpers
-    def publish_to(channel, &block)
-      message = {:channel => channel, :data => {:_eval => capture(&block)}, :ext => {:private_pub_token => PrivatePub.config[:secret_token]}}
+    def publish_to(channel, object = nil, &block)
+      message = {:channel => channel, :data => {}, :ext => {:private_pub_token => PrivatePub.config[:secret_token]}}
+      message[:data][:_data] = block_given? ? capture(&block) : object.to_json
       PrivatePub.publish(:message => message.to_json)
     end
 
-    def subscribe_to(channel)
+    def subscribe_to(channel, options = {})
       subscription = PrivatePub.subscription(:channel => channel)
-      content_tag :span, "", :class => "private_pub_subscription",
+      span_options = {
+        :class => "private_pub_subscription",
         "data-server" => PrivatePub.config[:server],
         "data-channel" => subscription[:channel],
         "data-signature" => subscription[:signature],
         "data-timestamp" => subscription[:timestamp]
+      }
+      span_options["data-callback"] = options[:callback] if options[:callback]
+      content_tag :span, "", span_options
     end
   end
 end
