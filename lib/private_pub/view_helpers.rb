e1947435ceaaabde6ede8d13bf12a11a921bf761
Ryan Bates <ryan@railscasts.com>
2011-04-08 17:11:42 -0700

    rewriting javascript so it's not dependent upon jQuery - closes #2

--- lib/private_pub/view_helpers.rb
+++ lib/private_pub/view_helpers.rb
@@ -2,17 +2,17 @@ module PrivatePub
   module ViewHelpers
     def publish_to(channel, object = nil, &block)
       message = {:channel => channel, :data => {:channel => channel}, :ext => {:private_pub_token => PrivatePub.config[:secret_token]}}
-      message[:data][:_data] = block_given? ? capture(&block) : object.to_json
+      message[:data][:eval] = capture(&block) if block_given?
+      message[:data][:data] = object if object
       PrivatePub.publish(:message => message.to_json)
     end
 
     def subscribe_to(channel)
       subscription = PrivatePub.subscription(:channel => channel)
-      content_tag :span, "", :class => "private_pub_subscription",
-        "data-server" => PrivatePub.config[:server],
-        "data-channel" => subscription[:channel],
-        "data-signature" => subscription[:signature],
-        "data-timestamp" => subscription[:timestamp]
+      subscription[:server] = PrivatePub.config[:server]
+      content_tag "script", :type => "text/javascript" do
+        raw("PrivatePub.sign(#{subscription.to_json});")
+      end
     end
   end
 end
