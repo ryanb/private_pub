06050926e088623fd8ba5f8fbe593fc12e3aa0a5
Brandon Tilley <brandon@brandontilley.com>
2011-11-22 16:24:37 -0800

    Only copy private_pub.js if Rails.version < 3.1

--- lib/generators/private_pub/install_generator.rb
+++ lib/generators/private_pub/install_generator.rb
@@ -1,29 +1,17 @@
-if ::Rails.version < "3.1"
-  module PrivatePub
-    module Generators
-      class InstallGenerator < Rails::Generators::Base
-        def self.source_root
-          File.dirname(__FILE__) + "/templates"
-        end
-
-        def copy_files
-          remove_file "config/initializers/private_pub.rb"
-          remove_file "app/helpers/private_pub_helper.rb"
-          template "private_pub.yml", "config/private_pub.yml"
-          copy_file "../../../../vendor/assets/javascripts/private_pub.js", "public/javascripts/private_pub.js"
-          copy_file "faye.ru", "faye.ru"
-        end
+module PrivatePub
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      def self.source_root
+        File.dirname(__FILE__) + "/templates"
       end
-    end
-  end
-else
-  module PrivatePub
-    module Generators
-      class InstallGenerator < Rails::Generators::Base
-        def do_nothing
-          say_status("deprecated", "You are using Rails 3.1, so this generator is no longer needed. The necessary files are already in your asset pipeline.")
-          say_status("", "Just add `//= require private_pub` to your app/assets/javascripts/application.js.")
-          say_status("", "If you upgraded your app from Rails 3.0 and still have private_pub.js in your javascripts, be sure to remove it.")
+
+      def copy_files
+        remove_file "config/initializers/private_pub.rb"
+        remove_file "app/helpers/private_pub_helper.rb"
+        template "private_pub.yml", "config/private_pub.yml"
+        copy_file "faye.ru", "faye.ru"
+        if ::Rails.version < "3.1"
+          copy_file "../../../../vendor/assets/javascripts/private_pub.js", "public/javascripts/private_pub.js"
         end
       end
     end
