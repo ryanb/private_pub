e71d0bab41ede6491b1238f1508a702495611efc
Ryan Bates <ryan@railscasts.com>
2012-01-14 15:09:02 -0800

    cleaning up install generator a bit

--- lib/generators/private_pub/install_generator.rb
+++ lib/generators/private_pub/install_generator.rb
@@ -6,8 +6,6 @@ module PrivatePub
       end
 
       def copy_files
-        remove_file "config/initializers/private_pub.rb"
-        remove_file "app/helpers/private_pub_helper.rb"
         template "private_pub.yml", "config/private_pub.yml"
         if ::Rails.version < "3.1"
           copy_file "../../../../app/assets/javascripts/private_pub.js", "public/javascripts/private_pub.js"
