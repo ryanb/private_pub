2a753a69a4ac0229c1947aeaa2e922d23231bd08
Diana Chow + Zhao Lu <dev+dc+zl@cotweet.com>
2012-03-05 19:41:31 -0800

    Revert "Added logic to check in Faye server is up.  Only subscribe when it is up.  Log a console error if it is not.  PrivatePub client now no longer throw uncaught exception and upon seeing console log error, client can choose to handle the situation by performing a null check on PrivatePub.fayeClient."
    We are violating cross-domain security check by making ajax call to Faye server.
    
    This reverts commit ff647fa0f47b1fa383604e71b2893fd116dcc7fb.

--- spec/javascripts/private_pub_spec.js
+++ spec/javascripts/private_pub_spec.js
@@ -1,22 +1,21 @@
-describe("PrivatePub", function () {
+describe("PrivatePub", function() {
   var pub, doc;
-  beforeEach(function () {
-    $ = { ajax: function(){} }
+  beforeEach(function() {
     Faye = {}; // To simulate global Faye object
     doc = {};
     pub = buildPrivatePub(doc);
   });
 
-  it("adds a subscription callback", function () {
+  it("adds a subscription callback", function() {
     pub.subscribe("hello", "callback");
     expect(pub.subscriptionCallbacks["hello"]).toEqual("callback");
   });
 
-  it("has a fayeExtension which adds matching subscription signature and timestamp to outgoing message", function () {
+  it("has a fayeExtension which adds matching subscription signature and timestamp to outgoing message", function() {
     var called = false;
-    var message = {channel:"/meta/subscribe", subscription:"hello"}
-    pub.subscriptions["hello"] = {signature:"abcd", timestamp:"1234"}
-    pub.fayeExtension.outgoing(message, function (message) {
+    var message = {channel: "/meta/subscribe", subscription: "hello"}
+    pub.subscriptions["hello"] = {signature: "abcd", timestamp: "1234"}
+    pub.fayeExtension.outgoing(message, function(message) {
       expect(message.ext.private_pub_signature).toEqual("abcd");
       expect(message.ext.private_pub_timestamp).toEqual("1234");
       called = true;
@@ -24,116 +23,65 @@ describe("PrivatePub", function () {
     expect(called).toBeTruthy();
   });
 
-  it("evaluates javascript in message response", function () {
-    pub.handleResponse({eval:'self.subscriptions.foo = "bar"'});
+  it("evaluates javascript in message response", function() {
+    pub.handleResponse({eval: 'self.subscriptions.foo = "bar"'});
     expect(pub.subscriptions.foo).toEqual("bar");
   });
 
-  it("triggers callback matching message channel in response", function () {
+  it("triggers callback matching message channel in response", function() {
     var called = false;
-    pub.subscribe("test", function (data, channel) {
+    pub.subscribe("test", function(data, channel) {
       expect(data).toEqual("abcd");
       expect(channel).toEqual("test");
       called = true;
     });
-    pub.handleResponse({channel:"test", data:"abcd"});
+    pub.handleResponse({channel: "test", data: "abcd"});
     expect(called).toBeTruthy();
   });
 
-  it("tries to reach faye server before subscribing", function(){
-    spyOn($, "ajax")
-    var options = {server:"server", channel:"somechannel"}
-    pub.sign(options)
-    expect($.ajax).toHaveBeenCalled()
-    expect($.ajax.mostRecentCall.args[0].url).toBe(options.server)
-  })
-
-  describe("when client can connect to faye server", function () {
-    beforeEach(function(){
-      spyOn($, 'ajax').andCallFake(function(options) {
-        options.success.call()
-      })
-    })
-
-    it("adds a faye subscription with response handler when signing", function () {
-      var faye = {subscribe:jasmine.createSpy()}
-
-      spyOn(pub, 'faye').andCallFake(function (callback) {
-        callback(faye);
-      });
-      var options = {server:"server", channel:"somechannel"};
-      pub.sign(options);
-      expect(faye.subscribe).toHaveBeenCalledWith("somechannel", pub.handleResponse);
-      expect(pub.subscriptions.server).toEqual("server");
-      expect(pub.subscriptions.somechannel).toEqual(options);
+  it("adds a faye subscription with response handler when signing", function() {
+    var faye = {subscribe: jasmine.createSpy()};
+    spyOn(pub, 'faye').andCallFake(function(callback) {
+      callback(faye);
     });
+    var options = {server: "server", channel: "somechannel"};
+    pub.sign(options);
+    expect(faye.subscribe).toHaveBeenCalledWith("somechannel", pub.handleResponse);
+    expect(pub.subscriptions.server).toEqual("server");
+    expect(pub.subscriptions.somechannel).toEqual(options);
+  });
 
-    it("adds a faye subscription with response handler when signing", function () {
-      var faye = {subscribe:jasmine.createSpy()};
-      spyOn(pub, 'faye').andCallFake(function (callback) {
-        callback(faye);
-      });
-      var options = {server:"server", channel:"somechannel"};
-      pub.sign(options);
-      expect(faye.subscribe).toHaveBeenCalledWith("somechannel", pub.handleResponse);
-      expect(pub.subscriptions.server).toEqual("server");
-      expect(pub.subscriptions.somechannel).toEqual(options);
+  it("adds a faye subscription with response handler when signing", function() {
+    var faye = {subscribe: jasmine.createSpy()};
+    spyOn(pub, 'faye').andCallFake(function(callback) {
+      callback(faye);
     });
-  })
+    var options = {server: "server", channel: "somechannel"};
+    pub.sign(options);
+    expect(faye.subscribe).toHaveBeenCalledWith("somechannel", pub.handleResponse);
+    expect(pub.subscriptions.server).toEqual("server");
+    expect(pub.subscriptions.somechannel).toEqual(options);
+  });
 
-  describe("when client cannot connect to faye server", function () {
-    beforeEach(function(){
-      spyOn($, 'ajax').andCallFake(function(options) {
-        options.error.call()
-      })
-    })
-
-    it("does not add a faye subscription with response handler when signing", function(){
-      var faye = {subscribe:jasmine.createSpy('faye')}
-      spyOn(pub, 'faye').andCallFake(function (callback) {
-        callback(faye);
-      });
-      var options = {server:"server", channel:"somechannel"};
-      pub.sign(options);
-      expect(faye.subscribe).not.toHaveBeenCalled()
-      expect(pub.subscriptions.server).not.toEqual("server");
-      expect(pub.subscriptions.somechannel).not.toEqual(options);
-    })
-
-    it("logs an error", function() {
-      var faye = {subscribe:jasmine.createSpy()}
-
-      spyOn(console, 'log')
-      spyOn(pub, 'faye').andCallFake(function (callback) {
-        callback(faye)
-      })
-      var options = {server:"server", channel:"somechannel"}
-      pub.sign(options)
-      expect(console.log).toHaveBeenCalledWith('Cannot connect to pubsub server')
-    })
-  })
-
-  it("triggers faye callback function immediately when fayeClient is available", function () {
+  it("triggers faye callback function immediately when fayeClient is available", function() {
     var called = false;
     pub.fayeClient = "faye";
-    pub.faye(function (faye) {
+    pub.faye(function(faye) {
       expect(faye).toEqual("faye");
       called = true;
     });
     expect(called).toBeTruthy();
   });
 
-  it("adds fayeCallback when client and server aren't available", function () {
+  it("adds fayeCallback when client and server aren't available", function() {
     pub.faye("callback");
     expect(pub.fayeCallbacks[0]).toEqual("callback");
   });
 
-  it("adds a script tag loading faye js when the server is present", function () {
+  it("adds a script tag loading faye js when the server is present", function() {
     script = {};
-    doc.createElement = function () {
-      return script;
-    };
-    doc.documentElement = {appendChild:jasmine.createSpy()};
+    doc.createElement = function() { return script; };
+    doc.documentElement = {appendChild: jasmine.createSpy()};
     pub.subscriptions.server = "path/to/faye";
     pub.faye("callback");
     expect(pub.fayeCallbacks[0]).toEqual("callback");
@@ -143,10 +91,10 @@ describe("PrivatePub", function () {
     expect(doc.documentElement.appendChild).toHaveBeenCalledWith(script);
   });
 
-  it("connects to faye server, adds extension, and executes callbacks", function () {
+  it("connects to faye server, adds extension, and executes callbacks", function() {
     callback = jasmine.createSpy();
-    client = {addExtension:jasmine.createSpy()};
-    Faye.Client = function (server) {
+    client = {addExtension: jasmine.createSpy()};
+    Faye.Client = function(server) {
       expect(server).toEqual("server")
       return client;
     };
